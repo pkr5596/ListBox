a10397a450daab58e7a3f86c547b28fa1cced9c4
Kiryang Park <pkr5596@gmail.com>
2019-06-03 09:05:42 +0900

    #2 Listbox에 항목을 추가하고, *** 코드 구현

--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -11,9 +11,43 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lbResult.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if(this.txtList.Text != "")
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("항목을 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            this.txtList.Focus();
+        }
+
+        private void TxtList_KeyPress(object sender, EventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                CheckText();
+                e.Handled = true;
+            }
+        }
+
+        private void LbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lbResult.Text = this.OrgStr + this.lbView.Text;
         }
     }
 }
